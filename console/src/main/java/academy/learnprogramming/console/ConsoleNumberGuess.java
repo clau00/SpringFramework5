a6df343d55352a6eb9ea4dd86027af0e6d35b8c1
claudiu <danbalint_88@yahoo.com>
2021-05-06 18:31:55 +0300

    CB: Spring5: changed to Constructor Dependency Injection, code clean up

--- console/src/main/java/academy/learnprogramming/console/ConsoleNumberGuess.java
+++ console/src/main/java/academy/learnprogramming/console/ConsoleNumberGuess.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Scanner;
 
+
 @Component
 public class ConsoleNumberGuess {
 
@@ -18,11 +19,15 @@ public class ConsoleNumberGuess {
     public static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
 
     // == fields ==
-    @Autowired
-    private Game game;
+    private final Game game;
+    private final MessageGenerator messageGenerator;
 
+    // == constructors ==
     @Autowired
-    private MessageGenerator messageGenerator;
+    public ConsoleNumberGuess(Game game, MessageGenerator messageGenerator) {
+        this.game = game;
+        this.messageGenerator = messageGenerator;
+    }
 
     // == events ==
     @EventListener(ContextRefreshedEvent.class)
