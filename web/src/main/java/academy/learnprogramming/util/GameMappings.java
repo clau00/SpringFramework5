e5e5ab0a40805a7289e82bf85ec77ab22a8e74a6
claudiu <danbalint_88@yahoo.com>
2021-05-21 14:51:00 +0300

    CB: Spring5: added Thymeleaf and internationalization(i18n)

--- web/src/main/java/academy/learnprogramming/util/GameMappings.java
+++ web/src/main/java/academy/learnprogramming/util/GameMappings.java
@@ -5,7 +5,7 @@ public final class GameMappings {
     // == constants ==
     public static final String PLAY = "play";
     public static final String REDIRECT_PLAY = "redirect:/" + PLAY;
-    public static final String RESTART = "restart";
+    public static final String RESTART = "game.restart";
     public static final String HOME = "/";
 
     // == constructors ==
